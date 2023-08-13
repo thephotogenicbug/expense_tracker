cf87c6cdcfea3b17627d177fe4982ff5894cea16
thephotogenicbug <navinkumarachari@gmail.com>
2023-08-13 21:48:19 +0530

    setup app theme with ColorScheme

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,22 @@
 import 'package:expense_tracker/widgets/expenses.dart';
 import 'package:flutter/material.dart';
 
+var kColorScheme = ColorScheme.fromSeed(
+  seedColor: const Color.fromARGB(255, 96, 59, 181),
+);
+
 void main() {
   runApp(
     MaterialApp(
-      theme: ThemeData(useMaterial3: true),
+      theme: ThemeData().copyWith(
+        useMaterial3: true,
+        colorScheme: kColorScheme,
+        appBarTheme: AppBarTheme().copyWith(
+          backgroundColor: kColorScheme.onPrimaryContainer,
+          foregroundColor: kColorScheme.primaryContainer,
+          
+        ),
+      ),
       home: const Expenses(),
     ),
   );
