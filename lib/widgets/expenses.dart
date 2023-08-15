bc0f0bd7033361669831861b56cdb8305b92d9ac
thephotogenicbug <navinkumarachari@gmail.com>
2023-08-15 11:52:50 +0530

    Bring media query

--- lib/widgets/expenses.dart
+++ lib/widgets/expenses.dart
@@ -66,6 +66,8 @@ class _Expenses extends State<Expenses> {
 
   @override
   Widget build(context) {
+    final width = MediaQuery.of(context).size.width;
+
     Widget mainContent = const Center(
       child: Text("No expenses found. Start adding some!"),
     );
@@ -87,12 +89,21 @@ class _Expenses extends State<Expenses> {
           ),
         ],
       ),
-      body: Column(
-        children: [
-          Chart(expenses: _registeredExpenses),
-          Expanded(child: mainContent)
-        ],
-      ),
+      body: width < 600
+          ? Column(
+              children: [
+                Chart(expenses: _registeredExpenses),
+                Expanded(child: mainContent)
+              ],
+            )
+          : Row(
+              children: [
+                Expanded(
+                  child: Chart(expenses: _registeredExpenses),
+                ),
+                Expanded(child: mainContent)
+              ],
+            ),
     );
   }
 }
